f646f34de0ae11b428472e88f84508b9537454cc
lee_c <gitleelee@gmail.com>
2023-12-17 20:24:57 -0800

    Clean-up

---
+++ src/UserDetails.js
@@ -0,0 +1,11 @@
+const UserDetails = () => (
+  <div className="user-details">
+    <p className="name "></p>
+    <p className="country"></p>
+    <p className="email"></p>
+    <figure>{/* Place image tag here */}</figure>
+    <button>Fetch Another</button>
+  </div>
+);
+
+export default UserDetails;
